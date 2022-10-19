94e0df2b81242d954cb1c9eb7b1e5635f4342b2d
coderstr <3420580499@qq.com>
2022-10-19 22:51:43 +0800

    帖子模块创建

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+import envConfig from '../environment/getEnvironmentCofing';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(envConfig.PORT, () => {
+    // 打印启动日志
+    Logger.log(`server open in localhost:${envConfig.PORT}`);
+  });
 }
 bootstrap();
 
