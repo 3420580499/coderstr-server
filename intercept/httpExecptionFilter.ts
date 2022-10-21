4750e46b50ef0c8091448181a04f03a92522723b
coderstr <3420580499@qq.com>
2022-10-21 17:36:53 +0800

    修改管道验证类抛出的异常格式

--- intercept/httpExecptionFilter.ts
+++ intercept/httpExecptionFilter.ts
@@ -10,18 +10,31 @@ import { Request, Response } from 'express';
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
+    console.log('异常过滤器执行');
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
     const message = exception.getResponse();
 
-    response.status(status).json({
-      statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-      error: message,
-      success: 'false',
-    });
+    // 不管是代码直接抛出异常还是在管道验证类中抛出的异常，都会来到这里
+    // 将管道验证类抛出的异常格式 来统一处理一下
+    if (typeof message === 'string') {
+      response.status(status).json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        error: message,
+        success: 'false',
+      });
+    } else {
+      response.status(status).json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        error: [...message['message']],
+        success: 'false',
+      });
+    }
   }
 }
